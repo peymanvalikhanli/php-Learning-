a549a701cb015aacd17a9ae70cbec22569434ae0
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-13 14:10:19 +0330

    Create p4.php

---
+++ session1/p4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // this var is color and equal red
+    $color = "red"; 
+    # print var of the color
+    echo "<p> this is color = ". $color . "</p>";
+    ?>
+</body>
+</html>
