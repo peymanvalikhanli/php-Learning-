36a320eb666dbb90ea3d7220c20f74ecc2b7e26b
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-13 14:23:14 +0330

    Create p6.php

---
+++ session1/p6.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $text = "this is a number : "; 
+    $num1 = 12; 
+    $text2 = "this is a float number : ";
+    $num2 = 12.555; 
+
+    echo "<p>".$text. $num1 . "</p>"; 
+    echo "<p>" . $text2 . $num2 . "</p>";
+    ?>
+</body>
+</html>
