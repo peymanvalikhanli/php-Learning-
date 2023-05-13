74bf5b4d31591c16534e907d779c458590e371e1
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-13 14:34:45 +0330

    Create p8.php

---
+++ session1/p8.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+    $x = 10; 
+    $y = 15; 
+
+    function test(){
+        global $x, $y; 
+        $y = $x + $y; 
+    }
+
+    test(); 
+
+    echo $y;
+?>
+</body>
+</html>
