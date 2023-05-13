cf201b91856b8ec4f09297269c169fe5afe1cb0e
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-13 14:28:57 +0330

    Create p7.php

---
+++ session1/p7.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $x = 5;
+
+        function test(){
+            $x = 50;
+            echo "<p> x() => ".$x."</p>";
+        }
+
+        test(); 
+
+        echo "<p> x => ".$x."</p>";
+    ?>
+
+</body>
+</html>
