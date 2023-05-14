f961c24380c0aadb7067cd47f960dd529254aee6
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-14 12:25:08 +0330

    Create p3.php

---
+++ session2/p3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $num1 = 23456; 
+    var_dump($num1); 
+    $num2 = 23456.1232; 
+    var_dump($num2); 
+    $x = true;
+    $y = false;
+    var_dump($x);
+    var_dump($y);
+    ?>
+</body>
+</html>
