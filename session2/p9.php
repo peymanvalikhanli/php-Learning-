6c3207d7716c2e21e67895e1f12b6640ff2dbade
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-14 14:09:04 +0330

    Update p9.php

--- session2/p9.php
+++ session2/p9.php
@@ -13,11 +13,14 @@
     echo "<br>pi <br>";
     echo (pi());
     echo "<br>max <br>";
-    echo "12, 11, 0, -1, 100 <br>"; 
+    echo "12, 11, 0, -1, 100 <br>";
     echo min(12, 11, 0, -1, 100);
     echo "<br>min <br>";
-    echo "200, 1, -1, 0, 12 <br>"; 
+    echo "200, 1, -1, 0, 12 <br>";
     echo max(200, 1, -1, 0, 12);
+    echo "<br>abs<br>";
+    echo "-1<br>";
+    echo abs(-1);
     ?>
 </body>
 
