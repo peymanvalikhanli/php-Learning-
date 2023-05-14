ef2a94d45e57e38c727315b111248587226836c0
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-14 14:07:03 +0330

    Update p9.php

--- session2/p9.php
+++ session2/p9.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-    echo(pi()); 
+    echo "<br>pi <br>";
+    echo (pi());
+    echo "<br>max <br>";
+    echo "12, 11, 0, -1, 100 <br>"; 
+    echo min(12, 11, 0, -1, 100);
+    echo "<br>min <br>";
+    echo "200, 1, -1, 0, 12 <br>"; 
+    echo max(200, 1, -1, 0, 12);
     ?>
 </body>
+
 </html>
