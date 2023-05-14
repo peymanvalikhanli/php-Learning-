8d31600035421e6b41450d5db080f24f30fa4754
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-14 12:30:44 +0330

    Create p4.php

---
+++ session2/p4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $arr = array("t1",123,123.456,true,false); 
+
+    var_dump($arr); 
+
+    echo "<br><hr><br>"; 
+
+    //print_r($arr); 
+
+    ?>
+</body>
+</html>
