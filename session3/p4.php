be3c0503a3f6197aacd34379ebe39d9bfe66715b
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-20 14:04:19 +0330

    Update p4.php

--- session3/p4.php
+++ session3/p4.php
@@ -1,24 +1,30 @@
 <?php
 
 
-    // URL sample : http://localhost/php-Learning-/session3/p3.php?act=even&num=20
+// URL sample : http://localhost/php-Learning-/session3/p3.php?act=even&num=20
 
-    $select = $_REQUEST["act"];
-    $stop = $_REQUEST["num"];
-    $num = 0;
-    switch (trim($select)) {
-        case "odd":
-        case "Odd":
-        case "ODd":
-        case "ODD":
-        case "oDD":
-            $num = 1;
-            break;
-        default:
-            $num = 0;
-    }
+$select = $_REQUEST["act"];
+$stop = $_REQUEST["num"];
+$num = 0;
+$data = array();
+$i = 0;
+switch (trim($select)) {
+    case "odd":
+    case "Odd":
+    case "ODd":
+    case "ODD":
+    case "oDD":
+        $num = 1;
+        break;
+    default:
+        $num = 0;
+}
 
-    while ($num <= $stop) {
-        echo $num, "<br>";
-        $num += 2;
-    }
+while ($num <= $stop) {
+    // echo $num, "<br>";
+    $data[$i] = $num;
+    $i++;
+    $num += 2;
+}
+
+print_r($data); 
