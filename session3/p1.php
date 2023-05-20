749bdeb013b56a4565c2e26a6c3cb11e88227d36
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-20 12:31:30 +0330

    Create p1.php

---
+++ session3/p1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $color = "red "; 
+
+        switch($color){
+            case "red":
+                echo "selected red";
+                break;
+            case "green":
+                echo "selected green";
+                break;
+            case "blue":
+                echo "selected blue";
+                break;
+            default:
+            echo "any";
+        }
+    ?>
+</body>
+</html>
