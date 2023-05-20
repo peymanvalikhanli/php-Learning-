3348a26cded23a054dd8bec842e887c6567f7182
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-20 12:34:42 +0330

    Update p1.php

--- session3/p1.php
+++ session3/p1.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        $color = "red "; 
+    $color = " red ";
 
-        switch($color){
-            case "red":
-                echo "selected red";
-                break;
-            case "green":
-                echo "selected green";
-                break;
-            case "blue":
-                echo "selected blue";
-                break;
-            default:
+    switch (trim($color)) {
+        case "red":
+            echo "selected red";
+            break;
+        case "green":
+            echo "selected green";
+            break;
+        case "blue":
+            echo "selected blue";
+            break;
+        default:
             echo "any";
-        }
+    }
     ?>
 </body>
+
 </html>
