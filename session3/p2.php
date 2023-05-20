9077c8ef5b46980a7e5a92d56a6b20d449909f03
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-20 12:51:49 +0330

    Update p2.php

--- session3/p2.php
+++ session3/p2.php
@@ -1,26 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        $num = 1; 
-        while($num <=5){
-            echo $num , "<br>";  
-            $num++; 
-        }
+    $num = 1;
+    while ($num <= 5) {
+        echo $num, "<br>";
+        $num++;
+    }
 
-        echo "<br><hr><br>"; 
+    echo "<br><hr><br>";
 
-        $num = 0; 
-        while($num <=100){
-            echo $num , "<br>";  
-            $num+=10; 
-        }
+    $num = 0;
+    while ($num <= 100) {
+        echo $num, "<br>";
+        $num += 10;
+    }
+
+    echo "<br><hr><br>";
+
+    $i = 0;
+
+    while ($i <= 100) {
+        if ($i % 2 == 0)
+            echo $i, "<br>";
+        $i++;
+    }
     ?>
 </body>
+
 </html>
