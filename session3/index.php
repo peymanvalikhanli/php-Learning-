3bfd65f2123a6d15293c43ebbca03da7acd5c007
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-20 12:22:40 +0330

    Update index.php

--- session3/index.php
+++ session3/index.php
@@ -1,25 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
-<?php 
-    //$h = Date("H"); 
 
+    <?php
+    $h = Date("H");
+
+    // $h = 9;
     // $h =10; 
-    $h = 23;
+    // $h = 23;
 
-    if($h < 20 ){
+    if ($h < 10) {
+        echo "Have a good morning ";
+    } else if ($h < 20) {
         echo "Have a good day";
-    }else {
+    } else {
         echo "have a good night!";
     }
-?> 
+    ?>
 
 </body>
+
 </html>
