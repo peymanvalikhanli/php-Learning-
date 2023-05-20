267df10a72c3cd49d8a09159e30ddf22b5fad377
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-20 13:48:54 +0330

    Update p3.php

--- session3/p3.php
+++ session3/p3.php
@@ -5,16 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP</title>
 </head>
 
 <body>
+
+    <form action="p3.php">
+        <input type="text" name="act" id="act" placeholder="odd|even">
+        <input type="number" id="num" name="num">
+        <input type="submit" value="submit">
+    </form>
+
     <?php
 
     // URL sample : http://localhost/php-Learning-/session3/p3.php?act=even&num=20
 
     $select = $_REQUEST["act"];
-    $stop = $_REQUEST["num"]; 
+    $stop = $_REQUEST["num"];
     $num = 0;
     switch (trim($select)) {
         case "odd":
@@ -28,9 +35,9 @@
             $num = 0;
     }
 
-    while($num <= $stop ){
-        echo $num,"<br>"; 
-        $num+=2; 
+    while ($num <= $stop) {
+        echo $num, "<br>";
+        $num += 2;
     }
 
     ?>
