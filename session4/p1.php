531830c0e68ed8602f91f42814c3e5e0c21dddfa
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-21 13:01:48 +0330

    Update p1.php

--- session4/p1.php
+++ session4/p1.php
@@ -5,3 +5,23 @@ function message(){
 }
 
 message(); 
+
+echo "<br>"; 
+
+function show_name($name){
+    echo "this name is : $name"; 
+}
+
+show_name("peyman");
+
+
+echo "<br>"; 
+
+declare(strict_types=1); 
+
+function sum(int $a , int $b){
+    return $a+$b; 
+}
+
+
+echo sum(12,"test");
