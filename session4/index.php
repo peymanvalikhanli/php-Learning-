feead97ee7edf2e93bd28cf32ef83097202db7b0
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-21 12:36:47 +0330

    Update index.php

--- session4/index.php
+++ session4/index.php
@@ -1,30 +1,30 @@
 <?php
 
-$x = 6; 
+$x = 6;
 
-do{
-    echo "number", $x, "<br>"; 
-    $x++; 
-}while($x<=5); 
+do {
+    echo "number", $x, "<br>";
+    $x++;
+} while ($x <= 5);
 
-echo "<hr><br>"; 
+echo "<hr><br>";
 
-for ($i= 0 ; $i <= 100 ; $i+=10){
-    echo "index ", $i, "<br>"; 
+for ($i = 0; $i <= 100; $i += 10) {
+    echo "index ", $i, "<br>";
 }
 
-echo "<hr><br>"; 
+echo "<hr><br>";
 
-$arr = array("a","b","c","d"); 
+$arr = array("a", "b", "c", "d");
 
-foreach($arr as $v){
-    echo "data : ", $v, "<br>"; 
+foreach ($arr as $v) {
+    echo "data : ", $v, "<br>";
 }
 
-echo "<hr><br>"; 
+echo "<hr><br>";
 
-$arr = array("name"=>"a","lastname"=>"b","age"=>"c","email"=>"d"); 
+$arr = array("name" => "a", "lastname" => "b", "age" => "c", "email" => "d");
 
-foreach($arr as $z => $v){
-    echo $z, " : ", $v, "<br>"; 
+foreach ($arr as $z => $v) {
+    echo $z, " : ", $v, "<br>";
 }
