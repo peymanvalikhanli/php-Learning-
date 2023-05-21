269f47a0e6f5e421867d0bf633454a1b1f48b842
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-21 12:30:02 +0330

    Update index.php

--- session4/index.php
+++ session4/index.php
@@ -12,3 +12,11 @@ echo "<hr><br>";
 for ($i= 0 ; $i <= 100 ; $i+=10){
     echo "index ", $i, "<br>"; 
 }
+
+echo "<hr><br>"; 
+
+$arr = array("a","b","c","d"); 
+
+foreach($arr as $v){
+    echo "data : ", $v, "<br>"; 
+}
