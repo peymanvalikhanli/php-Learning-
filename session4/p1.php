71bfc719395bdadde0a57b83d64d5dc91e45fa70
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-21 13:07:57 +0330

    Update p1.php

--- session4/p1.php
+++ session4/p1.php
@@ -17,13 +17,24 @@ show_name("peyman");
 
 echo "<br>"; 
 
-declare(strict_types=1); 
+//declare(strict_types=1); 
 
 function sum(int $a , int $b){
     return $a+$b; 
 }
 
 
-echo sum(12,"test");
-echo sum(12,12.5);
-echo sum(12,12);
+// echo sum(12,"test");
+// echo sum(12,12.5);
+//echo sum(12,12);
+
+echo "<br>"; 
+
+
+function sum_one(int $a , int $b=10){
+    return $a+$b; 
+}
+
+echo sum_one(12,11),"<br>"; 
+echo sum_one(2),"<br>"; 
+
