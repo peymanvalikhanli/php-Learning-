ac4bdde0953f935b723bec3c09c0b8edab0cb026
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-27 14:48:32 +0330

    update

--- session5/login/script.js
+++ session5/login/script.js
@@ -7,3 +7,55 @@ This is just the HTML / CSS of it but it uses wordpress's login system.
 Nice and Simple
 
 */
+
+const btn_login = () => {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    console.log(username);
+    console.log(password);
+
+    if (username == "" || username == null || username == undefined) {
+        alert("plz enter username");
+        return;
+    }
+
+
+    if (password == "" || password == null || password == undefined) {
+        alert("plz enter password");
+        return;
+    }
+
+    if (username.length >= 20) {
+        alert("username is not valid");
+        return;
+    }
+
+    if (password.length > 50 || password.length < 6) {
+        alert("password is not valid");
+        return;
+    }
+
+    var requestOptions = {
+        method: 'GET'
+    };
+
+    fetch("http://localhost/php-Learning-/session5/login/userAuth.php?act=login&username="+username+"&password="+password, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            switch(result.act){
+                case "login":
+                    document.write("Hello "+ username); 
+                break; 
+
+                case "error":
+                    alert(result.message);
+                break; 
+            }
+        })
+        .catch(error => {
+            console.log('error', error); 
+            alert("you have a error!!!");
+        });
+}
