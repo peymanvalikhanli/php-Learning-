9559de2b6171a03145bbe5ba0570fc4bd6780cbc
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-27 12:39:17 +0330

    Create p2.php

---
+++ session5/p2.php
@@ -0,0 +1,10 @@
+<?php 
+
+$users = array(
+    array("peyman","valikhanli",10,"peymanvalikhanli2012@gmail.com"), 
+    array("ali","valikhanli",20,"alivalikhanli2012@gmail.com"), 
+    array("pooya","valikhanli",40,"pooyavalikhanli2012@gmail.com"), 
+    array("nasim","valikhanli",68,"nasimvalikhanli2012@gmail.com")
+);
+
+print_r($users); 
