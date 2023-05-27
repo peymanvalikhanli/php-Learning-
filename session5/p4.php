e4ebd64a5773c16798b342864cc28539746254e4
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-27 13:14:18 +0330

    Update p4.php

--- session5/p4.php
+++ session5/p4.php
@@ -5,6 +5,10 @@
 // print_r($age); 
 
 
-$age = array("peyman"=> 30,"ali"=>24, "hasan"=> 17); 
-ksort($age); 
-print_r($age); 
+// $age = array("peyman"=> 30,"ali"=>24, "hasan"=> 17); 
+// ksort($age); 
+// print_r($age); 
+
+$age = array("peyman" => 30, "ali" => 54, "hasan" => 35);
+arsort($age);
+print_r($age);
