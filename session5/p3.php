1b71619f78d4b47c963f9d6074411e4ab17e6522
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-27 13:01:58 +0330

    Update p3.php

--- session5/p3.php
+++ session5/p3.php
@@ -1,5 +1,10 @@
 <?php 
 
-$numbers = array(12,13,44,1,2,7,3,4,9,10,11,12,33,43,234,0);
-sort($numbers);
-print_r($numbers); 
+// $numbers = array(12,13,44,1,2,7,3,4,9,10,11,12,33,43,234,0);
+// sort($numbers);
+// print_r($numbers); 
+
+$text = array("aa","ab","b", "c", "z", "AC","AA", "a12","a11","a1","a2");
+sort($text);
+print_r($text); 
+
