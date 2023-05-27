939923ef959019c49cc3494318718fe2a79835a4
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-27 12:23:51 +0330

    Update index.php

--- session5/index.php
+++ session5/index.php
@@ -1,5 +1,10 @@
 <?php 
 $arr = array("data1","data2","data3","data4");
 
+$arr_len = count($arr); 
 // print_r($arr); 
-echo count($arr); 
+//echo count($arr); 
+
+for($index=0; $index < $arr_len ; $index++){
+    echo "Index ",$index , " of array : ",$arr[$index], "<br>";
+}
