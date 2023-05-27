c6ea571015768aa329cfb8a1a3918149bb61485d
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-27 12:29:17 +0330

    Create p1.php

---
+++ session5/p1.php
@@ -0,0 +1,8 @@
+<?php 
+
+$age = array("peyman"=> 30,"ali"=>24, "hasan"=> 17); 
+
+
+echo "Peyman is ", $age["peyman"]," years old. <br>"; 
+echo "Ali is ", $age["ali"]," years old. <br>"; 
+echo "Hasan is ", $age["hasan"]," years old. <br>"; 
