7619462aec01030e22f558b1a998804babca4522
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-05-28 14:51:43 +0330

    update

---
+++ session6/model/users.php
@@ -0,0 +1,8 @@
+<?php 
+
+require_once("index.php"); 
+
+function get_users(){
+
+    return $GLOBALS["users"]; 
+}
--- session6/users/index.php
+++ session6/users/index.php
@@ -1,3 +1,21 @@
 <?php
-
-echo "hello user"; 
+require_once("../global/index.php");
+require_once("../model/users.php");
+//print_r($_SERVER["REQUEST_METHOD"]); 
+switch ($_SERVER["REQUEST_METHOD"]) {
+    case "GET":
+        $data = get_users(); 
+        json_res("user_list",$data); 
+        break;
+        case "POST": 
+            json_res("new_user","");
+            break; 
+        case "PUT": 
+            json_res("edit_user","");
+            break; 
+        case "DELETE": 
+            json_res("delete_user","");
+            break; 
+    default:
+        error_message("this action not found!!", 404);
+}
