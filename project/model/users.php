e4f67fb8843bdf431a520dfe2986ff7ca07a9ff9
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-06-10 14:49:29 +0330

    Update users.php

--- project/model/users.php
+++ project/model/users.php
@@ -1,7 +1,8 @@
 <?php
 
-
 require_once("DB.php");
+
+
 
 function get_users()
 {
@@ -12,12 +13,25 @@ function get_users_name()
 {
     return select_all("users", "name as Name");
 }
+
 function get_users_name_lastname()
 {
     return select_all("users", "name as Name , lastName as LastName");
 }
 
+function get_users_by_username($username)
+{
+    $users = select("users", null , "username like '$username'");
+    
+    return $users;
+}
 
+function get_users_by_token ($token)
+{
+    $users = select("users", null , "token like '$token'");
+    
+    return $users;
+}
 
 function user_login($username, $password)
 {
