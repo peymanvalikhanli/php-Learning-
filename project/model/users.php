036f601adf37f8f051827bc55670cbad0a6de72f
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-06-10 15:02:19 +0330

    Update users.php

--- project/model/users.php
+++ project/model/users.php
@@ -19,26 +19,44 @@ function get_users_name_lastname()
     return select_all("users", "name as Name , lastName as LastName");
 }
 
-function get_users_by_username($username)
+function get_user_by_username($username)
 {
-    $users = select("users", null , "username like '$username'");
-    
+    $users = select("users", null, "username like '$username'");
+
     return $users;
 }
 
-function get_users_by_token ($token)
+function get_user_by_token($token)
 {
-    $users = select("users", null , "token like '$token'");
-    
+    $users = select("users", null, "token like '$token'");
+
     return $users;
 }
 
-function user_login($username, $password)
-{
-    $users = select("users", "username , password, token", "username like '$username'");
-    foreach ($users as $user) {
-        if ($user["username"] == $username && $user["password"] == $password) {
-            return true;
+function user_login($username, $password, $otp = null)
+{ 
+    if ($otp == null) {
+        $users = select("users", "username , password, token , passError", "username like '$username'");
+        foreach ($users as $user) {
+            if ($user["username"] == $username && $user["password"] == $password) {
+                if ($user["passError"] < 5)
+                    return true;
+                else
+                    return "user_block";
+            }
+        }
+    } else {
+        $users = select("users", "username , password, token , passError, otpCode", "username like '$username'");
+        foreach ($users as $user) {
+            if ($user["username"] == $username && $user["password"] == $password) {
+                if ($user["passError"] < 5)
+                    if ($user["otpCode"] == $otp)
+                        return true;
+                    else
+                        return "user_OTP_error";
+                else
+                    return "user_block";
+            }
         }
     }
     return false;
