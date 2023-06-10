e07dcf6e86836dff16b0b97d0978a7515c15b0c4
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-06-10 14:45:55 +0330

    Update DB.php

--- project/model/DB.php
+++ project/model/DB.php
@@ -24,7 +24,7 @@ function check_connection($mysqli = null)
     }
 }
 
-function select_all($table)
+function select_all($table, $cols = null)
 {
     global $DB_user, $DB_pass, $DB_name, $DB_debug_mode;
 
@@ -32,7 +32,14 @@ function select_all($table)
 
     check_connection($mysqli);
 
-    $sql = "SELECT * FROM `$table`";
+    $sql = null;
+
+    if ($cols == null) {
+        $sql = "SELECT * FROM `$table`";
+    } else {
+        $sql = "SELECT $cols FROM `$table`";
+    }
+
     $result = $mysqli->query($sql);
 
     // Fetch all
@@ -54,7 +61,61 @@ function select_all($table)
         if ($DB_debug_mode)
             print_r($temp);
 
-        array_push($data,$temp);
+        array_push($data, $temp);
+    }
+
+    // Free result set
+    $result->free_result();
+
+    $mysqli->close();
+    return $data;
+}
+
+function select($table, $cols = null, $where = null)
+{
+    global $DB_user, $DB_pass, $DB_name, $DB_debug_mode;
+
+    $mysqli = new mysqli("localhost", $DB_user, $DB_pass, $DB_name);
+
+    check_connection($mysqli);
+
+    $sql = null;
+
+    if ($cols == null) {
+        $sql = "SELECT * FROM `$table`";
+    } else {
+        $sql = "SELECT $cols FROM `$table`";
+    }
+
+    if ($where != null) {
+        $sql .= " where " . $where;
+    }
+
+    if($DB_debug_mode)
+        echo $sql; 
+
+    $result = $mysqli->query($sql);
+
+    // Fetch all
+    $result->fetch_all(MYSQLI_ASSOC);
+
+    $data = array();
+
+    if ($DB_debug_mode)
+        echo $result->num_rows;
+
+    $row_count = $result->num_rows;
+    // Free result set
+    $result->free_result();
+
+    $result = $mysqli->query($sql);
+
+    for ($index = 0; $index < $result->num_rows; $index++) {
+        $temp =  $result->fetch_array(MYSQLI_ASSOC);
+        if ($DB_debug_mode)
+            print_r($temp);
+
+        array_push($data, $temp);
     }
 
     // Free result set
