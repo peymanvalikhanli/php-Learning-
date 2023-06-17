8707da843fd334e5fc224ab3812e0124fcd08f68
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-06-17 12:49:43 +0330

    Create p3.php

---
+++ session10/p3.php
@@ -0,0 +1,26 @@
+<?php
+$cookie_name = "name";
+$cookie_value = "peyman";
+//setcookie($cookie_name, $cookie_value, time() + (86400 + 30), "/");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        if (isset($_COOKIE["name"]))
+            echo $_COOKIE["name"];
+        else
+            echo "";
+        ?>
+    </h1>
+</body>
+
+</html>
