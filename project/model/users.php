7acb79424b4a2e1c127db379e1f13e17af2f3995
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2023-06-17 13:30:42 +0330

    Update users.php

--- project/model/users.php
+++ project/model/users.php
@@ -62,31 +62,88 @@ function user_login($username, $password, $otp = null)
 
 function user_register($username, $name, $lastName, $email, $mobile, $password)
 {
-    
-    $res =  insert("users", "`username`, `token`, `name`, `lastName`, `email`, `emailVerify`, `mobile`, `mobileVerify`, `password`, `passError`, `otpCode`, `otpStatusID`, `capchaCode`, `lastLogin`, `statusID`, `createdBy`, `creariationDate`, `modifyDate`",
-                     "'$username', ' 12', '$name', '$lastName', '$email', '0', '$mobile', '0', '$password', '0', '', '1', '', NULL, '1', 'php', current_timestamp(), current_timestamp()");
+
+    if ($username == null || $username == "" || strlen($username)) {
+        throw new Exception("Error username is not valid", 1);
+    }
+    if ($name == null || $name == "" || strlen($name) > 20) {
+        throw new Exception("Error name is nod valid", 1);
+    }
+    if ($lastName == null || $lastName == "" || strlen($lastName) > 20) {
+        throw new Exception("Error last name is not valid", 1);
+    }
+    if ($email == null || $email == "" || strlen($email) > 50) {
+        throw new Exception("Error Email is not valid", 1);
+    }
+    if ($mobile == null || $mobile == "" || strlen($mobile) > 15) {
+        throw new Exception("Error mobile is not valid", 1);
+    }
+    if ($password == null || $password == "" || strlen($password) > 50 || strlen($password) < 6) {
+        throw new Exception("Error password is not valid. password must include 6 to 50 char", 1);
+    }
+
+    $res =  insert(
+        "users",
+        "`username`, `token`, `name`, `lastName`, `email`, `emailVerify`, `mobile`, `mobileVerify`, `password`, `passError`, `otpCode`, `otpStatusID`, `capchaCode`, `lastLogin`, `statusID`, `createdBy`, `creariationDate`, `modifyDate`",
+        "'$username', ' 12', '$name', '$lastName', '$email', '0', '$mobile', '0', '$password', '0', '', '1', '', NULL, '1', 'php', current_timestamp(), current_timestamp()"
+    );
     return $res;
 }
 
-function insert_user()
+function insert_user($username, $name, $lastName, $email, $mobile, $password)
 {
-    $cols = array( 
-        "`username`","token", "name", "lastName", "email", "emailVerify", "mobile", "mobileVerify", "password", "passError", "otpCode", "otpStatusID", "capchaCode", "lastLogin", "statusID", "createdBy", "creariationDate", "modifyDate"
+
+    if ($username == null || $username == "" || strlen($username)) {
+        throw new Exception("Error username is not valid", 1);
+    }
+    if ($name == null || $name == "" || strlen($name) > 20) {
+        throw new Exception("Error name is nod valid", 1);
+    }
+    if ($lastName == null || $lastName == "" || strlen($lastName) > 20) {
+        throw new Exception("Error last name is not valid", 1);
+    }
+    if ($email == null || $email == "" || strlen($email) > 50) {
+        throw new Exception("Error Email is not valid", 1);
+    }
+    if ($mobile == null || $mobile == "" || strlen($mobile) > 15) {
+        throw new Exception("Error mobile is not valid", 1);
+    }
+    if ($password == null || $password == "" || strlen($password) > 50 || strlen($password) < 6) {
+        throw new Exception("Error password is not valid. password must include 6 to 50 char", 1);
+    }
+
+    $cols = array(
+        "`username`", "token", "name", "lastName", "email", "emailVerify", "mobile", "mobileVerify", "password", "passError", "otpCode", "otpStatusID", "capchaCode", "lastLogin", "statusID", "createdBy", "creariationDate", "modifyDate"
     );
     $values = array(
-        "'user2'", "' 12'", "'user2'", "'users2'", "'user12@user.com'", "'0'", "'0936896543'", "'0'", "'123789'", "'0'", "'1234'", "'1'", "'1234'", "NULL", "'1'", "'system'", "current_timestamp()", "current_timestamp()"
+        "'$username'", "'  '", "'$name'", "'$lastName'", "' $email'", "'0'", "'$mobile'", "'0'", "'$password'", "'0'", "''", "'1'", "''", "NULL", "'1'", "'system'", "current_timestamp()", "current_timestamp()"
     );
     $res = insert_by_array("users", $cols, $values);
     return $res;
 }
 
-function user_update_username(){
-    // UPDATE `users` SET `username` = 'user4', `lastLogin` = NULL WHERE `users`.`username` = 'user2';
-    $res = update("users","`username` = 'user5'"," `users`.`username` = 'user4'");
-    return $res; 
+function user_update_username($last_username, $new_username)
+{
+    if ($last_username == null || $last_username == "") {
+        throw new Exception("Error last_username is not set", 1);
+    }
+    if ($new_username == null || $new_username == "") {
+        throw new Exception("Error new username is not set", 1);
+    }
+    try {
+        $res = update("users", "`username` = '$new_username'", " `users`.`username` = '$last_username'");
+        return $res;
+    } catch (Exception $e) {
+        return $res;
+    }
 }
 
-function user_delete($username) {
-    $res = delete("users", "`users`.`username` = '$username'");
-    return $res; 
+function user_delete($username)
+{
+    try {
+        $res = delete("users", "`users`.`username` = '$username'");
+        return $res;
+    } catch (Exception $e) {
+        return $e;
+    }
 }
